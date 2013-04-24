8ea93f35a1ad3b83977bd762342b662b91f1c235
Jesse Cummins <me@jessecummins.com>
2013-04-24 17:38:35 -0400

    Updated to use a class

--- folder_compare.rb
+++ folder_compare.rb
@@ -3,45 +3,61 @@
 require 'fileutils'
 require 'digest/md5'
 
-# 2013-04-20
-# Jesse Cummins: https://github.com/jessc
+# 2013-04-24
+# Jesse Cummins
+# https://github.com/jessc
+
+=begin
+# Bug List / TODO:
+
+=end
+
+
+class FolderCompare
+  def initialize(folder_paths)
+    @file_list = []
+    folder_paths.each do |path|
+      if path.end_with? "/" then path += "**/*"
+      else path += "/**/*"
+      end
+      @file_list << Dir.glob(path)
+    end
+    @file_list.flatten!
+  end
+
+  def compare
+    all_files = {}
+
+    @file_list.each do |file|
+      mod_time = File.new(file).mtime.to_s
+      size = File.new(file).size.to_s
+      digest = Digest::MD5.hexdigest(mod_time + size)
+
+      unless all_files.include? digest
+        all_files[digest] = [file]
+      else
+        all_files[digest] << file
+      end
+    end
+
+    s = ""
+    all_files.each do |k, v|
+      if v.length > 1
+        v.each { |file_name| s += file_name + "\n" }
+        s += "\n"
+      end
+    end
+
+    return s + "Did something!"
+  end
+end
 
 
 folder_paths = []
-ARGV.each do |path|
-  folder_paths << path
+unless ARGV.empty?
+  ARGV.each { |path| folder_paths << path }
+else folder_paths << Dir.pwd
 end
 
+puts FolderCompare.new(folder_paths).compare
 
-file_list = []
-folder_paths.each do |path|
-  if path.end_with? "/" then path += "**/*"
-  else path += "/**/*"
-  end
-  file_list << Dir.glob(path)
-end
-file_list.flatten!
-
-
-all_files = {}
-
-file_list.each do |file|
-  mod_time = File.new(file).mtime.to_s
-  size = File.new(file).size.to_s
-  digest = Digest::MD5.hexdigest(mod_time + size)
-
-  unless all_files.include? digest
-    all_files[digest] = [file]
-  else
-    all_files[digest] << file
-  end
-end
-
-all_files.each do |k, v|
-  if v.length > 1
-    v.each { |file_name| puts file_name }
-    puts
-  end
-end
-
-puts "Did something!"
